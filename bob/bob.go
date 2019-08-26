c011156247fe0f0bd67d15f57c70e5bd4dee19ee
Matt S <msug403@gmail.com>
2019-08-26 19:44:04 -0400

    remove redundant check and handle with nested if/else - nets performance improvement. removed unneeded variable and set variables to lowercase as they are internal variables and not global

--- bob/bob.go
+++ bob/bob.go
@@ -5,22 +5,23 @@ import (
 	"strings"
 )
 
-var IsLetter = regexp.MustCompile(`[a-zA-Z]+`).MatchString
-var IsNumber = regexp.MustCompile(`[0-9]`).MatchString
-var IsValid = regexp.MustCompile(`[0-9a-zA-Z?!.]+`).MatchString
+var isLetter = regexp.MustCompile(`[a-zA-Z]+`).MatchString
+var isValid = regexp.MustCompile(`[0-9a-zA-Z?!.]+`).MatchString
 
 func Hey(s string) string {
-	if !IsValid(s) {
+	if !isValid(s) {
 		return "Fine. Be that way!"
 	}
 
 	w := ""
 	s = strings.TrimSpace(s)
 	switch {
-	case strings.ToUpper(s) == s && IsLetter(s) && !(s[len(s)-1:] == "?"):
-		w = "Whoa, chill out!"
-	case strings.ToUpper(s) == s && IsLetter(s):
-		w = "Calm down, I know what I'm doing!"
+	case strings.ToUpper(s) == s && isLetter(s):
+		if !(s[len(s)-1:] == "?") {
+			w = "Whoa, chill out!"
+		} else {
+			w = "Calm down, I know what I'm doing!"
+		}
 	case s[len(s)-1:] == "?":
 		w = "Sure."
 	default:
