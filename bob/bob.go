8fb36fee9db7a7a1ac3614642bc560903c3962c0
Matt S <msug403@gmail.com>
2019-08-26 17:04:52 -0400

    remove nested switches and move IsValid string check to fail first. small cleanup of declaring type of w variable twice

--- bob/bob.go
+++ bob/bob.go
@@ -8,36 +8,23 @@ import (
 var IsLetter = regexp.MustCompile(`[a-zA-Z]+`).MatchString
 var IsNumber = regexp.MustCompile(`[0-9]`).MatchString
 var IsValid = regexp.MustCompile(`[0-9a-zA-Z?!.]+`).MatchString
-var w string
 
 func Hey(s string) string {
+	if !IsValid(s) {
+		return "Fine. Be that way!"
+	}
+
 	w := ""
+	s = strings.TrimSpace(s)
 	switch {
-	//determine if a valid string
-	case IsValid(s):
-		//trim whitespace from valid string
-		s := strings.TrimSpace(s)
-		switch {
-		//
-		case !(s[len(s)-1:] == "?"):
-			switch {
-			case strings.ToUpper(s) == s && IsLetter(s):
-				w = "Whoa, chill out!"
-			default:
-				w = "Whatever."
-			}
-		case s[len(s)-1:] == "?":
-			switch {
-			case IsNumber(s):
-				w = "Sure."
-			case strings.ToUpper(s) == s && IsLetter(s):
-				w = "Calm down, I know what I'm doing!"
-			default:
-				w = "Sure."
-			}
-		}
+	case strings.ToUpper(s) == s && IsLetter(s) && !(s[len(s)-1:] == "?"):
+		w = "Whoa, chill out!"
+	case strings.ToUpper(s) == s && IsLetter(s):
+		w = "Calm down, I know what I'm doing!"
+	case s[len(s)-1:] == "?":
+		w = "Sure."
 	default:
-		w = "Fine. Be that way!"
+		w = "Whatever."
 	}
 	return w
 }
