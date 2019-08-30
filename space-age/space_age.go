9e852edf6472cf8b3f136e8bbefb671dc249bd21
Matt S <msug403@gmail.com>
2019-08-30 10:34:25 -0400

    updated solution for space-age. includes mapping values and calculating the answer based off a constant value given. minimized some code from additional function to performing in one line. performance gains from using mapped values

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -1,36 +1,23 @@
 package space
 
-import (
-	"math"
-)
+import "math"
 
-// take in the seconds and planet from the test and send the result back as float64
+const eY float64 = 31557600
+
+// Age will take in the seconds and planet from the test and send the result back as float64
 func Age(seconds float64, planet string) float64 {
-	// declare the variable that we will be dividing by
-	var planet_secs float64
-	// set the number of seconds for Earth year on each planet
-	if planet == "Earth" {
-		planet_secs = 31557600
-	} else if planet == "Mercury" {
-		planet_secs = 7600543.81992
-	} else if planet == "Venus" {
-		planet_secs = 19414149.052176
-	} else if planet == "Mars" {
-		planet_secs = 59354032.69008
-	} else if planet == "Jupiter" {
-		planet_secs = 374355659.124
-	} else if planet == "Saturn" {
-		planet_secs = 929292362.884
-	} else if planet == "Uranus" {
-		planet_secs = 2651370019.3296
-	} else if planet == "Neptune" {
-		planet_secs = 5200418560.032
+	// map all values to one variable that we can reuse in the calculation
+	planetSecs := map[string]float64{
+		"Earth":   1,
+		"Mercury": 0.2408467,
+		"Venus":   0.61519726,
+		"Mars":    1.8808158,
+		"Jupiter": 11.862615,
+		"Saturn":  29.447498,
+		"Uranus":  84.016846,
+		"Neptune": 164.79132,
 	}
-	// do the calculation of seconds alive divided by number of seconds on that planet equal to Earth year, then round it
-	return (rounding(seconds / planet_secs))
-}
 
-// round the result of the calculation to the nearest hundredth and pass the value back as float64
-func rounding(in_round float64) float64 {
-	return (math.Round(in_round*100) / 100)
+	// do calculation against the result of the planet (from the main function) returned back from map
+	return (math.Round((seconds/(eY*planetSecs[planet]))*100) / 100)
 }
