bd72171f9225cec4470c4974626c0c28d9811e74
Matt S <msug403@gmail.com>
2019-09-09 21:04:37 -0400

    removed unneeded counting in raindrop solution

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -6,28 +6,21 @@ import (
 )
 
 // Convert will check if a number has 3, 5, or 7 as a factor
+// define the strings for each factorial. then create the variable for building the string result
+// integer array of factorials to use. then create variable used to count instances of non-factorial.
+// finally, add string if factorial. and, return the combined string if factorial match(es) exist
+// return original number if no factorial match
 func Convert(inNum int) string {
-	// define the strings for each factorial
 	output := map[int]string{3: "Pling", 5: "Plang", 7: "Plong"}
-	// create the variable for building the string result
 	var message strings.Builder
-	// integer array of factorials to use
 	factNum := []int{3, 5, 7}
-	// create variable used to count instances of non-factorial
-	var notFact int
 	for _, num := range factNum {
-		// add string if factorial
 		if inNum%num == 0 {
 			message.WriteString(output[num])
-		} else {
-			// increase notFact if not factorial
-			notFact++
 		}
 	}
-	// return number if no factorial match
-	if notFact == 3 {
+	if message.String() == "" {
 		return strconv.Itoa(inNum)
 	}
-	// return the combined string if factorial match(es) exist
 	return message.String()
 }
